ad0b51b7c1336ef11f2df1fb03eda9f45b9fcbbe
DimaP <workdmytropat@gmail.com>
2025-10-04 23:37:12 +0300

    Start making a JWT

--- Leopard_Web_Store/Leopard_Web_Store/settings.py
+++ Leopard_Web_Store/Leopard_Web_Store/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "store"
+    "django.contrib.auth",
+    "store",
+    "rest_framework",
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
---
+++ Leopard_Web_Store/store/tests/test_model.py
@@ -0,0 +1,3 @@
+import pytest
+from store.models import *
+
